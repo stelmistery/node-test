d7c44655aab3af1004773c10fccfcbc3bc83ebd6
Vladislav <levelup996@gmail.com>
2021-02-25 16:48:05 +0300

    added file api: search line, write text in file

--- app.js
+++ app.js
@@ -114,6 +114,29 @@ fileApi.get('/write', jsonParser, (request, response) => {
     response.json('Write file success');
 });
 
+fileApi.get('/search', jsonParser, (request, response) => {
+
+    let searchText = request.query.text;
+    try {
+        var fileContent = fs.readFileSync('testFile.txt', 'utf8');
+    } catch (err) {
+        console.log('Write file error');
+        throw err;
+    }
+    console.log(searchText);
+    let fileContentSplited = fileContent.split('\n');
+    var obj = {};
+    obj.data = {};
+    fileContentSplited.forEach((currentLine, ind) => {
+        if (currentLine.match(new RegExp(searchText, 'g'))) {
+            obj.data[(ind+1).toString()] = currentLine;
+        }
+    });
+    response.status(200);
+    response.set('Content-Type', 'text/json');
+    response.json(obj);
+});
+
 
 app.listen(3000, () => {
     console.log('Сервер слушает...')
